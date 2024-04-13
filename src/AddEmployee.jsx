07739422b5776f6ba828a8acd20f24076816e2d0
Smriti Gupta <91653836+SMR-GUP@users.noreply.github.com>
2024-04-14 01:25:00 +0530

    Update AddEmployee.jsx

--- src/AddEmployee.jsx
+++ src/AddEmployee.jsx
@@ -65,7 +65,7 @@ if (providedDate > today) {
 }
 
       console.log("Dataaa  ",data);
-        axios.post('https://ems-server-production-7c2f.up.railway.app/create', data)
+        axios.post('https://ems-server-production.onrender.com/create', data)
           .then(res => {
               navigate('/employee');
           })
